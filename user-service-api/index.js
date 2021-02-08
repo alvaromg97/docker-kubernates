7ce495b79ab5ace445680da4c1a97ec1daa283d3
alvaromg97 <almuino@gmail.com>
2021-02-08 15:58:54 +0100

    Initial commit

---
+++ user-service-api/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.json([
+    {
+    name: 'Steven',
+    email: 'steven.d.hondt.sdh@gmail.com'
+    },
+    {
+        name: 'Testies',
+        email: 'testies@gmail.com'
+    },
+    {
+        name: 'john',
+        email: 'john@doe.com'
+    },
+    {
+        name: 'Maria',
+        email: 'maria@doe.com'
+    }
+]))
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
